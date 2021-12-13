5c51cdca4a0c72960b973f9dfd2237defbaf8149
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-12-13 17:27:53 +0900

    feat: redisの簡単な実装の追加

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,83 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  # GET /articles or /articles.json
+  def index
+    @articles = cache_articles
+    # ids = REDIS.zrevrangebyscore "articles/daily/#{Date.today.to_s}", "+inf", 0, limit: [0, 5]
+    # @ranking_articles = ids.map{ |id| Article.find(id) } 
+  end
+
+
+  # GET /articles/1 or /articles/1.json
+  def show
+    @article = Article.find(params[:id])
+    # REDIS.incr "posts/daily/#{Date.today.to_s}/#{@post.id}"
+    # REDIS.incr "articles/daily/#{Date.today.to_s}/#{@article.id}"
+    REDIS.zincrby "articles/daily/#{Date.today.to_s}", 1, "#{@article.id}"
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles or /articles.json
+  def create
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles/1 or /articles/1.json
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: "Article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles/1 or /articles/1.json
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+    
+    def cache_articles
+      Rails.cache.fetch("cache_articles", expires_in: 60.minutes) do
+        Article.all.to_a
+      end
+    end
+end
+
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,10 @@
 # Redis.current = Redis.new
 # Redis.new(:host => '127.0.0.1', :port => 6379)
-Redis.current = Redis.new(:host => 'redis', :port => 6379)
+# Redis.current = Redis.new(:host => 'redis', :port => 6379)
+require 'redis'
+
+REDIS = Redis.new(host: 'redis', port: 6379)
+# REDIS = Redis.new(host: 'redis', port: 6379)
+# uri = URI.parse(ENV["REDIS"])
+# REDIS = Redis.new(host: uri.host, port: uri.port)
+# Redis.current = Redis.new(:host => 'redis', :port => 6379)
